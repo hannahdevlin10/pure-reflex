e811c25192f510e328b12c11c73b4fcb11d107d3
Hannah Devlin <hannahdevlin@Hannahs-MacBook-Pro-3.local>
2022-11-09 11:59:09 +0000

    unused var fix.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import CoverPhotoShort from "../CoverPhotoShort";
-import Desc from "./Desc";
 
 const About = () => {
     return (
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,23 @@
 import React from "react";
-import CardList from "../CardList.js";
 import CoverPhotoShort from "../CoverPhotoShort";
 
-const email = [
-    {
-        icon: 'phone',
-        content: 'jenniferamd54@gmail.com',
-        cardWidth: '30%',
-        link: '/'
-    },
-]
+// const email = [
+//     {
+//         icon: 'phone',
+//         content: 'jenniferamd54@gmail.com',
+//         cardWidth: '30%',
+//         link: '/'
+//     },
+// ]
 
-const profile = [
-    {
-        title: 'Profile',
-        cardWidth: '70%',
-        content: 'I have always had an interest in Reflexology and experienced its  many benefits over the years.  Following more than 20 years in the teaching profession both in Ireland and the UK, I decided to qualify as a Reflexologist. I obtained my ITEC Diploma with Distinction in Reflexology, and in Anatomy and Physiology from the Holistic College Dublin. I have also completed courses in Facial Reflexology and in Reflexology for Menopause. My speciality is in Women’s Health and Wellbeing. I am a member of the Irish Reflexologists Institute and am fully insured. Becoming a Reflexologist was the right decision for me and I look forward to the next chapter. I believe that in order for us to take care of others, we need to take care of ourselves first.',
-        link: '/'
-    }
-]
+// const profile = [
+//     {
+//         title: 'Profile',
+//         cardWidth: '70%',
+//         content: 'I have always had an interest in Reflexology and experienced its  many benefits over the years.  Following more than 20 years in the teaching profession both in Ireland and the UK, I decided to qualify as a Reflexologist. I obtained my ITEC Diploma with Distinction in Reflexology, and in Anatomy and Physiology from the Holistic College Dublin. I have also completed courses in Facial Reflexology and in Reflexology for Menopause. My speciality is in Women’s Health and Wellbeing. I am a member of the Irish Reflexologists Institute and am fully insured. Becoming a Reflexologist was the right decision for me and I look forward to the next chapter. I believe that in order for us to take care of others, we need to take care of ourselves first.',
+//         link: '/'
+//     }
+// ]
 
 const Contact = () => {
     return (
