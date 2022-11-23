bee1bbfcd210926ce03159730aeece50de9ca932
Hannah Devlin <hannahdevlin@Hannahs-MacBook-Pro-3.local>
2022-11-23 17:29:29 +0000

    redundant import removed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Route, Switch, useLocation, BrowserRouter as Router, } from "react-router-dom";
+import { Route, Switch, BrowserRouter as Router, } from "react-router-dom";
 import './App.css';
 import Nav from './components/Nav';
 import Drawer from './components/Drawer';
