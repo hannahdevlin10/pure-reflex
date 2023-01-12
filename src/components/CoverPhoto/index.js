54f8ca777f3e8249e43a4396266adc28d45a2684
Hannah Devlin <hannahdevlin@Hannahs-MacBook-Pro-3.local>
2023-01-12 11:03:16 +0000

    location added to main.

--- src/components/CoverPhoto/index.js
+++ src/components/CoverPhoto/index.js
@@ -1,7 +1,8 @@
-import { Button } from "@material-ui/core";
 import React from "react";
 import { Link } from "react-router-dom";
+import { Button } from "@material-ui/core";
 import lotusPic from '../../assets/water-lily-dark.png'
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import './style.css';
 
 const CoverPhoto = () => {
@@ -19,6 +20,19 @@ const CoverPhoto = () => {
                 </div>
                 <div className="slogan">
                     Relax | Rebalance | Restore
+                </div>
+                <div
+                    className="location"
+                    style={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                        }}>
+                    <div>
+                        <LocationOnIcon style={{ fontSize: 25 }} />
+                    </div>
+                    <div style={{ marginLeft: 3 }}>
+                       Terenure, South Dublin
+                    </div>
                 </div>
                 <div className="buttonsWrapper">
                     <Link to='/contact'>
